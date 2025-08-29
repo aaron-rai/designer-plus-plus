9db34cb7c160cb4a1c27e4581ae071c7d111aff5
aaron-rai <aaron.rai@bwdesigngroup.com>
2025-08-29 10:04:39 -0700

    removed some unused things

--- gateway/src/main/java/org/dev/arai/designerpp/gateway/utils/RunScriptConsoleCode.java
+++ gateway/src/main/java/org/dev/arai/designerpp/gateway/utils/RunScriptConsoleCode.java
@@ -1,28 +1,22 @@
 package org.dev.arai.designerpp.gateway.utils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import org.dev.arai.designerpp.common.DesignerPlusPlusConstants;
-import org.python.core.Py;
-import org.python.core.PyCode;
-import org.python.core.PyObject;
-import org.python.core.PySystemState;
-import org.python.util.PythonInterpreter;
+import org.python.core.PyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.inductiveautomation.ignition.common.script.JythonExecException;
+import com.inductiveautomation.ignition.common.script.ScriptFunction;
 import com.inductiveautomation.ignition.common.script.ScriptManager;
-import com.inductiveautomation.ignition.gateway.model.GatewayContext;
+import com.inductiveautomation.ignition.common.script.ScriptManager.UndefinedFunctionException;
 
 public class RunScriptConsoleCode {
-    private static final Logger logger =
-        LoggerFactory.getLogger(DesignerPlusPlusConstants.MODULE_ID + ".RunScriptConsoleCode");
+    private static final Logger logger = LoggerFactory.getLogger(DesignerPlusPlusConstants.MODULE_ID + ".RunScriptConsoleCode");
 
-    public PyCode convertToPyCode(String code) {
-        try (PythonInterpreter interpreter = new PythonInterpreter()) {
-            return interpreter.compile(code, "<GatewayConsole>");
-        }
+    public void runCode(ScriptManager scriptManager, String code) throws PyException, JythonExecException, UndefinedFunctionException {
+        logger.info("Running script code: {}", code);
+
+        ScriptFunction func = scriptManager.compileFunction(code);
+		scriptManager.runFunction(func, null);
     }
 }
